293ae53873202ede4dbfc65289d7dd541a53b6cf
Sam Boysel <sboysel@gmail.com>
2016-07-13 03:48:57 -0400

    read directly from nyc opendata; keep only borough name

--- data-raw/nycboroughs.R
+++ data-raw/nycboroughs.R
@@ -1,5 +1,22 @@
+library(sp)
 library(rgdal)
-boroughs <- rgdal::readOGR(dsn = "data-raw/nycboroughs.geojson",
-                           layer = "OGRGeoJSON")
 
+# Download from NYC OpenData
+# https://data.cityofnewyork.us/City-Government/Borough-Boundaries/tqmj-j8zm
+tmp <- tempfile(fileext = ".geojson")
+download.file(url = "https://data.cityofnewyork.us/resource/6jcb-t2g6.geojson",
+              destfile = tmp,
+              method = "curl")
+boroughs <- rgdal::readOGR(dsn = tmp,
+                           layer = "OGRGeoJSON")
+unlink(tmp)
+
+# Only keep name
+boroughs@data <- boroughs@data["boro_name"]
+names(boroughs) <- "name"
+
+# Change boroughs feature IDs
+boroughs <- sp::spChFIDs(boroughs, as.character(boroughs$name))
+
+# Save
 save(boroughs, file = "data/nycboroughs.RData", compress = "xz")
